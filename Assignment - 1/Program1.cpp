411c08d71985adaef77ab20327ac8d3f2800fe2a
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-03-19 16:31:24 +0530

    Setup done and added 2 programs

---
+++ Assignment - 1/Program1.cpp
@@ -0,0 +1,19 @@
+// Write a program to print "Jay Ganesh" on screen.
+
+//-----------------------------------------------------------
+// Header file and Macros
+//-----------------------------------------------------------
+#include<iostream>
+using namespace std;
+
+//----------------------------------------------
+// Description   :  This function Display the output using cout funtion
+// Author        :  Sneha Yogesh Choudhari
+//----------------------------------------------
+int main()
+{
+	cout<<"Jay Ganesh from C++ \n ";
+	
+	return 0;
+	
+}
